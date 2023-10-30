8b8760c57a3e04ba1f9c92ff9ef7cb205616abdd
vladim <vladim@cobuilder.no>
2023-10-30 13:22:49 +0200

    upgrade packages

--- games/src/app/components/tetris/tetris-home/tetris-home.component.ts
+++ games/src/app/components/tetris/tetris-home/tetris-home.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-tetris-home',
   templateUrl: './tetris-home.component.html',
-  styleUrls: ['./tetris-home.component.scss']
+  styleUrls: ['./tetris-home.component.scss'],
 })
-export class TetrisHomeComponent {
-
+export class TetrisHomeComponent implements OnInit {
+  ngOnInit(): void {
+    interval(1000)
+      .pipe()
+      .subscribe((x) => {
+        console.log(x);
+      });
+  }
 }
